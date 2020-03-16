de5a56c97357f889f041d596aa5e630baaab334c
Mohammad <m.alzebaq@gmail.com>
2020-03-16 14:25:17 +0100

    Objects-function-alert

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,9 @@
-let europäischeUnion = {
-    Belgien: "BE", Griechenland: "EL", Litauen: "LT", Portugal: "PT", Bulgarien: "BG", Spanien: "ES", Luxemburg: "LU", Rumänien: "RO", Tschechien: "CZ", Frankreich: "FR", Ungarn: "HU", Slowenien: "SI", Dänemark: "DK", Kroatien: "HR", Malta: "MT", Slowakei: "SK", Deutschland: "DE", Italien: "IT", Niederlände: "NL", Finnland: "FI", Estland: "EE", Zypern: "CY", Österreich: "AT", Schweden: "SE", Irland: "IE", Lettland: "LV", Polen: "PL", VereinigtesKönigreich: "UK",
+let person = {
+    name: "mohammad alzibak",
+    alter: "24",
+    sagNameAlter: () => {
+        console.log(`my name is ${person.name} my age is ${person.alter}`)
+        alert(name, alter)
+    }
 }
-console.log(europäischeUnion)
+person.sagNameAlter()
